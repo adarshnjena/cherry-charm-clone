494319f63a17dfbf0f58ee924941eeecc72c0b4c
Adarsh Jena <76236137+adarshnjena@users.noreply.github.com>
2023-11-26 17:39:43 +0530

    Add react-icons package for additional icons support

--- src/interface/Interface.tsx
+++ src/interface/Interface.tsx
@@ -17,6 +17,7 @@ import {
   ModalFooter,
   Button,
   useDisclosure,
+  Input,
 } from "@nextui-org/react";
 
 const Interface = () => {
@@ -114,19 +115,18 @@ const Interface = () => {
                     Nullam pulvinar risus non risus hendrerit venenatis.
                     Pellentesque sit amet hendrerit risus, sed porttitor quam.
                   </p>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Nullam pulvinar risus non risus hendrerit venenatis.
-                    Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                  </p>
-                  <p>
-                    Magna exercitation reprehenderit magna aute tempor cupidatat
-                    consequat elit dolor adipisicing. Mollit dolor eiusmod sunt
-                    ex incididunt cillum quis. Velit duis sit officia eiusmod
-                    Lorem aliqua enim laboris do dolor eiusmod. Et mollit
-                    incididunt nisi consectetur esse laborum eiusmod pariatur
-                    proident Lorem eiusmod et. Culpa deserunt nostrud ad veniam.
-                  </p>
+                  <Input
+                    isRequired
+                    type="name"
+                    label="Name"
+                    className="max-w"
+                  />
+                  <Input
+                    isRequired
+                    type="email"
+                    label="Email"
+                    className="max-w"
+                  />
                 </ModalBody>
                 <ModalFooter>
                   <Button color="danger" variant="light" onPress={onClose}>
@@ -139,7 +139,7 @@ const Interface = () => {
                       onClose();
                     }}
                   >
-                    Action
+                    Get Spins !!
                   </Button>
                 </ModalFooter>
               </>
